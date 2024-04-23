39856a267f0f8c4cd506db55dbc2c66e797346f0
Brucedevnairobi <bruce14simiyu@gmail.com>
2024-04-23 10:30:41 +0300

    Remove site-logo.svg, replace with new logo, update Navbar text color, and fix typo in middleware
    * Delete app/site-logo.svg
    * Add public/assets/images/site-logo.svg

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return <div className="text-white">I rep the Navigation Page</div>;
+  return (
+    <nav>
+      <Link href="/">
+        <Image
+          src="/assets/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow"
+        />
+        <p>Dev <span>Overflow</span></p>
+      </Link>
+      GlobalSearch
+      <div>
+        Theme
+        <SignedIn>
+          <UserButton 
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: 'h-10 w-10'
+            },
+            variables: {
+              colorPrimary: '#ff7000'
+            }
+          }}
+          />  
+        </SignedIn>
+        MobileNav
+
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
